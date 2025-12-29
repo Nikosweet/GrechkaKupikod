d60e03372290e00949e25d5dda23d3952d92605c
Nikosweet <kupchikov1975@mail.ru>
2025-12-29 22:40:03 +0700

    index.html

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,10 +1,19 @@
+require('dotenv').config();
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
-module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.js'),
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
-        clean: true
+module.exports = (env) => {
+    return {
+        mode: env.mode ?? 'development',
+        entry: path.resolve(__dirname, 'src', 'index.js'),
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'bundle.[contenthash].js',
+            clean: true
+        },
+        plugins: [
+            new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')})
+        ],
+        
     }
 }
